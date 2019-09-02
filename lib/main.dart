b15a92c5ef7831a8a565106ec26bcc1884fe3e9d
HIEU Le Trung <hieuletrung102@gmail.com>
2019-09-02 19:48:07 +0700

    floating button

--- lib/main.dart
+++ lib/main.dart
@@ -11,32 +11,81 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TextDemo(),
     );
   }
 }
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+String longtext =
+    '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas auctor
+vel ligula eget fermentum. Integer mattis nulla vitae ullamcorper
+dignissim. Donec vel velit vel eros lobortis laoreet at sit amet turpis.
+Ut in orci blandit, rhoncus metus quis, finibus augue. Nullam a elit
+vel ligula eget fermentum. Integer mattis nulla vitae ullamcorper
+dignissim. Donec vel velit vel eros lobortis laoreet at sit amet turpis.
+Ut in orci blandit, rhoncus metus quis, finibus augue. Nullam a elit
+vel ligula eget fermentum. Integer mattis nulla vitae ullamcorper
+dignissim. Donec vel velit vel eros lobortis laoreet at sit amet turpis.
+Ut in orci blandit, rhoncus metus quis, finibus augue. Nullam a elit
+vel ligula eget fermentum. Integer mattis nulla vitae ullamcorper
+dignissim. Donec vel velit vel eros lobortis laoreet at sit amet turpis.
+Ut in orci blandit, rhoncus metus quis, finibus augue. Nullam a elit
+vel ligula eget fermentum. Integer mattis nulla vitae ullamcorper
+dignissim. Donec vel velit vel eros lobortis laoreet at sit amet turpis.
+Ut in orci blandit, rhoncus metus quis, finibus augue. Nullam a elit
+vel ligula eget fermentum. Integer mattis nulla vitae ullamcorper
+dignissim. Donec vel velit vel eros lobortis laoreet at sit amet turpis.
+Ut in orci blandit, rhoncus metus quis, finibus augue. Nullam a elit
+vel ligula eget fermentum. Integer mattis nulla vitae ullamcorper
+dignissim. Donec vel velit vel eros lobortis laoreet at sit amet turpis.
+Ut in orci blandit, rhoncus metus quis, finibus augue. Nullam a elit
+vel ligula eget fermentum. Integer mattis nulla vitae ullamcorper
+dignissim. Donec vel velit vel eros lobortis laoreet at sit amet turpis.
+Ut in orci blandit, rhoncus metus quis, finibus augue. Nullam a elit
+venenatis metus accumsan dapibus. Vestibulum imperdiet tristique viverra.''';
 
-  final String title;
+class TextDemo extends StatelessWidget {
+  // static const routeName = '/material/text';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('Terms and Conditions'),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello, World!',
-            ),
+      body: pad(
+        ListView(
+          children: [
+            pad(Text('Single line of text')),
+            Divider(),
+            pad(Text('     Text with       a   lot of    whitespace     ')),
+            Divider(),
+            pad(Text(
+                'Text with a newline\ncharacter should render in 2 lines')),
+            Divider(),
+            pad(Text(
+              longtext,
+            )),
+            Divider(),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('pressed');
+        },
+        child: Icon(Icons.send),
+      ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
+
+  Padding pad(Widget child) => Padding(
+        padding: EdgeInsets.all(12),
+        child: child,
+      );
 }
