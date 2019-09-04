84f3aa86234a42e520032bb5b02099c9ba58a488
HIEU Le Trung <hieuletrung102@gmail.com>
2019-09-04 15:27:30 +0700

    refactor files

--- lib/firstroute.dart
+++ lib/firstroute.dart
@@ -1,56 +1,6 @@
 import 'package:flutter_web/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
-import 'data.dart';
-
-class PostData extends StatefulWidget {
-  @override
-  _PostDataState createState() => _PostDataState();
-}
-
-class _PostDataState extends State<PostData> {
-  Future<Post> post;
-
-  void _handlePress() {
-    setState(() {
-      post = fetchPost();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          Container(
-            width: 400,
-            height: 200,
-            alignment: Alignment.center,
-            color: Colors.amberAccent,
-            child: FutureBuilder<Post>(
-              future: post,
-              builder: (context, snapshot) {
-                // print(snapshot);
-                if (snapshot.hasData) {
-                  return Image.asset(pic[snapshot.data.ticketId]);
-                } else if (snapshot.hasError) {
-                  return Text('${snapshot.error}');
-                }
-                // return CircularProgressIndicator();
-                return Text('Lucky Draw ');
-              },
-            ),
-          ),
-          RaisedButton(
-            child: Text('Draw Now!'),
-            onPressed: _handlePress,
-          ),
-        ],
-      ),
-    );
-  }
-}
+import 'widget/post.dart';
 
 class FirstRoute extends StatelessWidget {
   FirstRoute({Key key, this.title, this.post}) : super(key: key);
@@ -89,44 +39,5 @@ class FirstRoute extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class Post {
-  final String ticketId;
-  final String rewardId;
-
-  Post({this.ticketId, this.rewardId});
-
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return Post(
-        ticketId: json['result']['ticketId'],
-        rewardId: json['result']['reward']['id']);
-    //
-    // Example draw result
-    // {
-    //   "code": 200,
-    //   "result": {
-    //     "ticketId": "d9f1adca-7fc2-4282-af33-d9c5a2b46a9a",
-    //     "reward": {
-    //       "id": "d290f1ee-6c54-4b01-90e6-d701748f0851"
-    //     }
-    //   }
-    // }
-  }
-}
-
-Future<Post> fetchPost() async {
-  final response = await http.post(url);
-  print(response);
-
-  if (response.statusCode == 200) {
-    // If server returns an OK response, parse the JSON.
-    var body = jsonDecode(response.body);
-    // print(body);
-    return Post.fromJson(body);
-  } else {
-    // If that response was not OK, throw an error.
-    throw Exception('Failed to load post');
   }
 }
