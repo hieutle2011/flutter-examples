55ae856b7d8a6ea242a659951881bc7b5d3ddf00
HIEU Le Trung <hieuletrung102@gmail.com>
2019-09-09 14:16:45 +0700

    update project name

--- web/main.dart
+++ web/main.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 import 'package:flutter_web_ui/ui.dart' as ui;
-import 'package:init/main.dart' as app;
+import 'package:lucky_draw_app/main.dart' as app;
 
 main() async {
   await ui.webOnlyInitializePlatform();
