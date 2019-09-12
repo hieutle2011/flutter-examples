226c73e5325113e7eeebd76cbbb5299e15b90672
HIEU Le Trung <hieuletrung102@gmail.com>
2019-09-12 08:18:41 +0700

    initialize app

---
+++ lib/joke.dart
@@ -0,0 +1,35 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Joke {
+  final String id;
+  final String content;
+  final int status;
+
+  Joke({this.id, this.content, this.status});
+
+  factory Joke.fromJson(Map<String, dynamic> json) {
+    if (json['status'] == 200) {
+      return Joke(
+        id: json['id'],
+        content: json['joke'],
+        status: json['status'],
+      );
+    } else {
+      print(json['status']);
+      return Joke(id: null, content: null, status: json['status']);
+    }
+  }
+}
+
+Future<Joke> fetchJoke() async {
+  String url = 'https://icanhazdadjoke.com/';
+  Map<String, String> header = {"Accept": "application/json"};
+  http.Response response = await http.get(url, headers: header);
+  if (response.statusCode == 200) {
+    var body = json.decode(response.body);
+    return Joke.fromJson(body);
+  } else {
+    throw Exception('Failed to load joke');
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,94 @@
 import 'package:flutter_web/material.dart';
 
+import 'joke.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-  final String title;
+class _MyHomePageState extends State<MyHomePage> {
+  Future<Joke> joke;
+
+  @override
+  void initState() {
+    super.initState();
+    joke = fetchJoke();
+  }
+
+  void _newJoke() {
+    setState(() {
+      joke = fetchJoke();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('widget.title'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Hello, World!',
+              'You have pushed the button this many times:',
             ),
+            JokePanel(joke: joke),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _newJoke,
+        tooltip: 'New joke!',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class JokePanel extends StatelessWidget {
+  final Future<Joke> joke;
+
+  JokePanel({Key key, this.joke}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: joke,
+      builder: (context, snapshot) {
+        // we have a joke
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData && snapshot.data.status == 200) {
+            // String id = snapshot.data.id;
+            String jokeContent = snapshot.data.content;
+            // int status = snapshot.data.status;
+            return Container(
+              child: Text(jokeContent),
+            );
+
+            // something goes wrong
+          } else if (snapshot.hasError) {
+            return Container(
+              child: Text('${snapshot.error}'),
+            );
+          }
+        }
+
+        // we are still loading
+        return CircularProgressIndicator();
+      },
     );
   }
 }
