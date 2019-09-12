3c2d7202e7c71385a566f08c6daa61fff14c816f
HIEU Le Trung <hieuletrung102@gmail.com>
2019-09-12 16:20:03 +0700

    add working sample

--- lib/main.dart
+++ lib/main.dart
@@ -11,31 +11,71 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
   final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String message = "abc";
+  ScrollController _controller;
+
+  _scrollListener() {
+    if (_controller.offset >= _controller.position.maxScrollExtent) {
+      setState(() {
+        message = 'reach the bottom';
+      });
+    } else if (_controller.offset <= _controller.position.minScrollExtent) {
+      setState(() {
+        message = 'reach the top';
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    _controller = ScrollController();
+    _controller.addListener(_scrollListener);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text("Scroll Limit reached"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello, World!',
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 50.0,
+            color: Colors.green,
+            child: Center(
+              child: Text(message),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              reverse: true,
+              controller: _controller,
+              itemCount: 15,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(Icons.message),
+                  title: Text("Index : $index"),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
