039a29f6cee6ae04ffcfc62cd0684b5f7fba82f2
HIEU Le Trung <hieuletrung102@gmail.com>
2019-09-12 21:20:41 +0700

    add class User

---
+++ lib/user.dart
@@ -0,0 +1,10 @@
+class User {
+  final String id;
+  final String name;
+
+  User({this.id, this.name});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(id: json['id'], name: json['name']);
+  }
+}
